51c17674f72189744d7d5b0528889876486ca22a
cristiano <cristianodevsystem@gmail.com>
2020-12-12 23:38:42 -0300

    Create index.js
    Implementação do link interno do JavaScript

---
+++ index.js
@@ -0,0 +1,26 @@
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+// Montando o JSon
+var user = {};
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+
+            user[field.name] = field.value;
+        }
+        
+       
+
+    } else {
+
+        user[field.name] = field.value;
+    }      
+
+});
+
+console.log(user);
+
+
