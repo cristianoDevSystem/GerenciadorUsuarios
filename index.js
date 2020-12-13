9c8186f16db6b3c4e380f0bc4228a779e7d3fa34
cristiano <cristianodevsystem@gmail.com>
2020-12-13 00:44:08 -0300

    Update index.js
    Crianção do evento "click" no botão, com o preenchimento do formulário e envio das informações de SPA (Single Page Application) e mudando o coportamento padrão da página através do preventDefault()

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 // Montando o JSon
 var user = {};
 
-fields.forEach(function(field, index){
+// Evento de Clique
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
 
-    if (field.name == "gender") {
+    event.preventDefault();
 
-        if (field.checked) {
+    fields.forEach(function(field, index){
 
+        if (field.name == "gender") {
+    
+            if (field.checked) {
+    
+                user[field.name] = field.value;
+            }
+            
+           
+    
+        } else {
+    
             user[field.name] = field.value;
-        }
-        
-       
-
-    } else {
-
-        user[field.name] = field.value;
-    }      
-
+        }      
+    
+    });
+    
+    console.log(user);
+    
 });
-
-console.log(user);
-
-
