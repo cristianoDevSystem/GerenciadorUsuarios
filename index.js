1d134cbab7852e666ffa3dfd81c0ba20f63fa724
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-05-08 11:16:50 -0300

    Correcação

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-// Recebendo o ID do formulario
-let userController = new UserController("form-user-create", "table-users");
-
+let userController = new UserController("form-user-create", "table-users")
