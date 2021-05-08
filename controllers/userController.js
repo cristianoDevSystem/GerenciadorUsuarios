4767cc96309d8f9746544b337970af2b5f06774b
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-05-08 11:53:30 -0300

    recuperando os dados para edição do formulario

--- controllers/userController.js
+++ controllers/userController.js
@@ -165,8 +165,27 @@ class UserController {
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
             let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
 
-            for (let)
+            // Pecorrendo cada um dos objetos
+            for (let name in json) {
+
+             let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+             
+             if (field) {
+                
+                switch (field.type) {
+                    case 'file':
+                        continue;
+                        break;
+                    case 'radio'
+                }
+
+                field.value = json[name];
+             }
+
+            }
 
             this.showPanelUpdate();
 
