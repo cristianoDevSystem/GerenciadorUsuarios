ed58f427e3a31c6fac47b161460b49fc77538e4e
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-05-14 20:51:27 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 let userController = new UserController("form-user-create", "form-user-update", "table-users");
-=======
-let userController = new UserController("form-user-create", "form-user-update", "table-users")
->>>>>>> Stashed changes
